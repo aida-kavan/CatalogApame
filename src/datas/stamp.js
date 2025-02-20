e6cad62772c4738cfb0feacc5d17fb51cfe658c7
aida-kavan <aidahajikavan@gmail.com>
2025-02-20 18:50:49 +0330

    vgsdfv

--- src/datas/stamp.js
+++ src/datas/stamp.js
@@ -3,79 +3,77 @@ export const stamp1 = [
     size: "13 * 43 ",
 
     code: "MF12",
-    price: "490،000",
+    price: "550،000",
   },
   {
     size: "17 * 43 ",
 
     code: "MF18",
-    price: "510،000",
+    price: "600،000",
   },
   {
     size: "22 * 55 ",
 
     code: "MF34",
-    price: "570،000",
+    price: "650،000",
   },
   {
     size: "27 * 46 ",
 
     code: "MF38",
-    price: "600،000",
+    price: "690،000",
   },
   {
     size: "28 * 55 ",
 
     code: "MF44",
-    price: "620،000",
+    price: "710،000",
   },
   {
     code: "MF 48",
     size: "17 * 59 ",
-
-    code: "MF48",
-    price: "570،000",
+    price: "650،000",
   },
   {
     size: "35 * 59 ",
 
     code: "MF60",
-    price: "690،000",
+    price: "750،000",
   },
   {
     size: "43 * 78 ",
 
     code: "MF62",
-    price: "720،000",
+    price: "790،000",
   },
   {
     size: "32 * 32 ",
 
     code: "MF32",
-    price: "520،000",
+    price: "650،000",
   },
   {
     size: "42 * 42 ",
 
     code: "MF40",
-    price: "630،000",
+    price: "730،000",
   },
   {
     size: "قطر 32 ",
 
     code: "MFR32",
-    price: "560،000",
+    price: "650،000",
   },
   {
     size: "قطر 42 ",
 
     code: "MFR42",
-    price: "630،000",
+    price: "720،000",
   },
   {
     size: "30 * 50 ",
     code: "MF-oval",
-    price: "650،000",
+    price: "790،000",
   },
 ];
 
@@ -83,109 +81,109 @@ export const stampRectangleMobi = [
   {
     size: "10 * 27 ",
     code: "C10-A",
-    price: "650،000",
+    price: "270،000",
     desc: "درپوش دار جاکلیدی",
   },
   {
     size: "10 * 27 ",
     code: "C10",
-    price: "650،000",
+    price: "280،000",
     desc: "درپوش دار",
   },
   {
     size: "14 * 38 ",
     code: "C20",
-    price: "650،000",
+    price: "310،000",
     desc: "درپوش دار",
   },
   {
     size: "18 * 50 ",
     code: "C30",
-    price: "650،000",
+    price: "320،000",
     desc: "درپوش دار",
   },
   {
     size: "22 * 58 ",
     code: "C40",
-    price: "650،000",
+    price: "370،000",
     desc: "درپوش دار",
   },
   {
     size: "26 * 64 ",
     code: "C45",
-    price: "650،000",
+    price: "420،000",
     desc: "درپوش دار",
   },
   {
     size: "31 * 70 ",
     code: "50",
-    price: "650،000",
+    price: "570،000",
     desc: "قفل دار",
   },
   {
     size: "39 * 77 ",
     code: "60",
-    price: "650،000",
+    price: "620،000",
     desc: "قفل دار",
   },
   {
     size: "25 * 45 ",
     code: "C52",
-    price: "650،000",
+    price: "520،000",
     desc: "درپوش دار",
   },
   {
     size: "30 * 50 ",
     code: "C53",
-    price: "650،000",
+    price: "540،000",
     desc: "درپوش دار",
   },
   {
     size: "40 * 50 ",
     code: "C54",
-    price: "650،000",
+    price: "630،000",
     desc: "درپوش دار",
   },
   {
     size: "40 * 58 ",
     code: "260",
-    price: "650،000",
+    price: "630،000",
     desc: "درپوش دار",
   },
   {
     size: "14 * 38 ",
     code: "intro1",
-    price: "650،000",
+    price: "250،000",
     desc: "بدون درپوش ",
   },
   {
     size: "18 * 50 ",
     code: "intro2",
-    price: "650،000",
+    price: "270،000",
     desc: "بدون درپوش ",
   },
   {
     size: "22 * 58 ",
     code: "intro3",
-    price: "650،000",
+    price: "290،000",
     desc: "بدون درپوش ",
   },
   {
     size: "48 * 90 ",
     code: "4316",
-    price: "650،000",
+    price: "1،100،000",
     desc: "قفل دار ",
   },
   {
     size: "10 * 70 ",
     code: "4916",
-    price: "650،000",
-    desc: "قفل دار ",
+    price: "330،000",
+    desc: "مهر شبا ",
   },
   {
     size: "12 * 12 ",
     code: "921",
-    price: "650،000",
+    price: "390،000",
     desc: "مربع ",
   },
   {
@@ -197,61 +195,61 @@ export const stampRectangleMobi = [
   {
     size: "30 * 30 ",
     code: "C930",
-    price: "650،000",
+    price: "410،000",
     desc: "مربع درپوش دار",
   },
   {
     size: "40 * 40 ",
     code: "C940",
-    price: "650،000",
+    price: "470،000",
     desc: "مربع درپوش دار",
   },
   {
     size: "قطر 12 ",
     code: "RC12",
-    price: "650،000",
+    price: "450،000",
     desc: "درپوش دار دایره",
   },
   {
     size: "قطر 17 ",
     code: "RC17",
-    price: "650،000",
+    price: "460،000",
     desc: "درپوش دار دایره",
   },
   {
     size: "قطر 24 ",
     code: "RC24",
-    price: "650،000",
+    price: "480،000",
     desc: "درپوش دار دایره",
   },
   {
     size: "قطر 30 ",
     code: "RC30",
-    price: "650،000",
+    price: "490،000",
     desc: "قفل درپوش دار دایره",
   },
   {
     size: "قطر 40 ",
     code: "RC40",
-    price: "650،000",
+    price: "520،000",
     desc: "قفل درپوش دار دایره",
   },
   {
     size: "قطر 50 ",
     code: "RC50",
-    price: "650،000",
+    price: "700،000",
     desc: "قفل درپوش دار دایره",
   },
   {
     size: "30 * 49 ",
     code: "C3050",
-    price: "650،000",
+    price: "520،000",
     desc: "درپوش دار بیضی",
   },
   {
     size: "40 * 60 ",
     code: "C4060",
-    price: "650،000",
+    price: "620،000",
     desc: "درپوش دار بیضی",
   },
 ];
@@ -260,37 +258,37 @@ export const pocketStamp = [
   {
     size: "14 * 41",
     code: "IDEAL 50",
-    price: "",
+    price: "420،000",
     desc: "مهر جیبی",
   },
   {
     size: "14 * 41",
     code: "SIRDAS 50",
-    price: "",
+    price: "420،000",
     desc: "مهر جیبی",
   },
   {
     size: "19 * 50",
     code: "IDEAL 80",
-    price: "",
+    price: "450،000",
     desc: "مهر جیبی",
   },
   {
     size: "19 * 51",
     code: "SIRDAS 80",
-    price: "",
+    price: "450،000",
     desc: "مهر جیبی",
   },
   {
     size: "32 * 32",
     code: "IDEAL 32",
-    price: "",
+    price: "490،000",
     desc: "مهر جیبی",
   },
   {
     size: "42 * 42",
     code: "IDEAL 42",
-    price: "",
+    price: "540،000",
     desc: "مهر جیبی",
   },
 ];
@@ -299,19 +297,19 @@ export const datastamp1 /* تاریخ زن دستی */ = [
   {
     size: "4mm",
     code: "1010",
-    price: "",
+    price: "220،000",
     desc: "تاریخ زن دستی",
   },
   {
     size: "5mm",
     code: "1020",
-    price: "",
+    price: "270،000",
     desc: "تاریخ زن دستی",
   },
   {
     size: "30 * 45",
     code: "2800",
-    price: "",
+    price: "320،000",
     desc: "تاریخ زن دستی",
   },
 ];
@@ -320,88 +318,95 @@ export const datestamp2 /* مهر پرینتی تاریخ زن اتوماتیک 
   {
     size: "38 * 77",
     code: "D60",
-    price: "780،000",
+    price: "520،000",
     desc: "عنوان و تاریخ زن اتوماتیک",
   },
   {
     size: "38 * 77",
     code: "D62",
-    price: "",
+    price: "780،000",
     desc: "عنوان و دو تاریخ زن اتوماتیک",
   },
   {
     size: "38 * 77",
     code: "DN62",
-    price: "",
+    price: "780،000",
     desc: "عنوان و تاریخ زن و نمراتور",
   },
   {
     size: "40 * 58",
     code: "D265",
-    price: "",
+    price: "520،000",
     desc: "عنوان و تاریخ زن اتوماتیک",
   },
   {
     size: "قطر 40",
     code: "DR40",
-    price: "",
+    price: "470،000",
     desc: "عنوان و تاریخ زن اتوماتیک دایره",
   },
   {
     size: "30 * 50",
     code: "D53",
-    price: "",
+    price: "550،000",
     desc: "عنوان و تاریخ زن اتوماتیک",
   },
   {
     size: "3.8 mm",
     code: "5810",
-    price: "",
+    price: "350،000",
     desc: " تاریخ زن اتوماتیک",
   },
+  {
+    size: "5 mm",
+    code: "5815",
+    price: "370،000",
+    desc: " تاریخ زن اتوماتیک",
+  },
+  {
+    size: "3.8 mm",
+    code: "5820",
+    price: "380،000",
+    desc: " تاریخ زن عنوان دار",
+  },
+
 ];
 
 export const numberstamp /* دستی */ = [
   {
     size: "5 mm",
     code: "1656",
-    price: "",
+    price: "420،000",
     desc: "شش رقم",
   },
   {
     size: "5 mm",
     code: "1658",
-    price: "",
+    price: "430،000",
     desc: "هشت رقم",
   },
   {
     size: "5 mm",
     code: "15510",
-    price: "",
+    price: "470،000",
     desc: "ده رقم",
   },
   {
     size: "5 mm",
     code: "15512",
-    price: "",
+    price: "490،000",
     desc: "دوازده رقم",
   },
   {
     size: "9 mm",
     code: "1598",
-    price: "",
-    desc: "هشت رقم",
-  },
-  {
-    size: "9 mm",
-    code: "1598",
-    price: "",
+    price: "520،000",
     desc: "هشت رقم",
   },
   {
     size: "9 mm",
     code: "15910",
-    price: "",
+    price: "590،000",
     desc: "ده رقم",
   },
 ];
@@ -410,13 +415,13 @@ export const numberstamp2 = [
   {
     size: "3.8 mm",
     code: 5830,
-    price: "",
+    price: "440،000",
     desc: "َشش رقم",
   },
   {
     size: "30 * 50",
     code: "N53",
-    price: "",
+    price: "490،000",
     desc: "شش رقم",
   },
 ];

26c357ff6335ec0b21c8424dd5a9baaab0140164
aida-kavan <aidahajikavan@gmail.com>
2025-04-21 19:35:23 +0330

    type error

--- src/pages/giftPages/GreenGifts.jsx
+++ src/pages/giftPages/GreenGifts.jsx
@@ -13,7 +13,7 @@ return (
     <div className={styles.mainPageCont}>
         <div className={styles.productCont}>
             <Descreption title={"چرا هدیه سبز را انتخاب کنید؟"} main={"بذر این محصولات به صورت رندوم از سبزیجات معطر مثل ریحان سبز، ریحان بنفش، شاهی، مرزه و جعفری است. در صورت درخواست بذر گل می‌توانید از بذرهای عسل یا مرواریدی، خردل زرد، مغربی، نیلوفر، ناز، میمون و اطلسی سفارش دهید. توجه داشته باشید که این بذرها، سفارشی و برای بالای صد عدد است و قیمت بالاتر می‌باشد."}/>
-            <Title title={"شمع ها"}/>
+            <Title title={"هدایای سبز"}/>
             {green.map((item) => (
                 <SquareProductItem img={item.image} code={item.Pcode} title={item.name}  desc={item.desc}/>
             ))}

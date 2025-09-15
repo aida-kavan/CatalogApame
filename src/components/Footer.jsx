0721bc59f3726c675b2ba0181939b5f8c888f55c
aida-kavan <aidahajikavan@gmail.com>
2025-09-15 12:28:28 +0330

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,29 @@
 import React from 'react'
 import styles from "../components.style/page.module.scss"
-import { Home, PhoneCall ,MapPin } from 'lucide-react'
-import {Link} from "react-router-dom"
+import { Home, PhoneCall, MapPin } from 'lucide-react'
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
     <footer>
-        <Link to={"/"}>
-        <Home color='#11176b'/>
-        </Link>
-        <a target="_blank"  href="tel:02634001215">
-        <PhoneCall color='#11176b'/>
-        </a>
-        <a target="_blank" href="https://balad.ir/p/6Y7fXmb8EJT1lJ">
-        <MapPin src="\icons\whatsapp.svg" color='#11176b'/>
-        </a>
-        <a  target="_blank" href="https://t.me/apameagency">
+      <Link to={"/"}>
+        <Home color='#11176b' />
+      </Link>
+      <a target="_blank" href="tel:02634001215">
+        <PhoneCall color='#11176b' />
+      </a>
+      <a target="_blank" href="https://balad.ir/p/6Y7fXmb8EJT1lJ">
+        <MapPin src="\icons\whatsapp.svg" color='#11176b' />
+      </a>
+      <a target="_blank" href="https://t.me/apameagency">
         <img src="\icons\telegram.svg" />
-        </a>
-        <a target="_blank"  href="https://wa.me/+989033020318">
+      </a>
+      <a target="_blank" href="https://wa.me/+989033020318">
         <img src="\icons\whatsapp.svg" />
-        </a>
-        <a target="_blank"  href="https://www.instagram.com/apametabligh/">
+      </a>
+      <a target="_blank" href="https://www.instagram.com/apameagency/">
         <img src="\icons\instagram.svg" />
-        </a>
+      </a>
     </footer>
   )
 }
